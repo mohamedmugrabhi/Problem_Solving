dd349e2f94ea935d8ac222747f117d35ab2d4a52
Mohamed_Mugrabhi <mohamed.mamdouh.mugrabhi@gmail.com>
2026-01-15 01:13:43 +0200

    Create I. Welcome for you with Conditions.cpp

---
+++ ICPC Assiut - Newcomers/Sheet #1 (Data type - Conditions)/I. Welcome for you with Conditions.cpp
@@ -0,0 +1,15 @@
+#include <iostream>   
+using namespace std;
+
+int main()
+{
+    
+  double n, m;
+    cin >> n >> m;
+    if (n >= m)
+        cout << "Yes";
+    else
+        cout << "No";
+    
+    return 0;
+}

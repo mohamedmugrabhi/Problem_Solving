a97dcd88e4d1b937c5c4c6c56335740a6ba72fe6
Mohamed_Mugrabhi <mohamed.mamdouh.mugrabhi@gmail.com>
2026-01-15 01:04:31 +0200

    Create D. Difference.cpp

---
+++ ICPC Assiut - Newcomers/Sheet #1 (Data type - Conditions)/D. Difference.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    long long x, y, z, d;
+    cin >> x >> y >> z >> d;
+    cout << "Difference" <<" = "<< (x * y) - (z * d);
+
+    return 0;
+}

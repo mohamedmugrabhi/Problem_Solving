bea95036b0ce1ee4249d72f706cee8ddfb903e85
Mohamed_Mugrabhi <mohamed.mamdouh.mugrabhi@gmail.com>
2026-01-20 21:28:27 +0200

    Create Z. Hard Compare.cpp

---
+++ ICPC Assiut - Newcomers/Sheet #1 (Data type - Conditions)/Z. Hard Compare.cpp
@@ -0,0 +1,20 @@
+#include <iostream>  
+#include<iomanip>    
+#include <string>    
+#include <algorithm>
+#include <cmath> 
+using namespace std;
+
+int main()
+{
+    long long a,b,c,d;
+    cin >> a >> b >> c >> d;
+    if (b * log(a) > d * log(c)) {
+        cout << "YES";
+    }
+    else
+        cout << "NO";
+    
+    return 0;
+}
+ 

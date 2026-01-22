a9cb69d73e82194b1c4dfebbfe59406db776b259
Mohamed_Mugrabhi <mohamed.mamdouh.mugrabhi@gmail.com>
2026-01-22 21:08:48 +0200

    Create L. GCD.cpp

---
+++ ICPC Assiut - Newcomers/Sheet #2  (Loops)/L. GCD.cpp
@@ -0,0 +1,21 @@
+#include <iostream>  
+  #include <string>
+   #include <cmath>
+    #include <algorithm>
+      #include<iomanip>
+using namespace std;
+int main()
+{
+    int n,y,z;
+    cin >> n>>y;
+    for (int i = 1; i <= n; i++) {
+        if (n % i == 0&&y%i==0)
+           { for (int j = 1; j <= i; j++) {
+                    z = j;
+            }}
+    }
+    cout << z;
+  
+    return 0;
+}
+ 

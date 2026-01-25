7669bea4681b47eef74f0bd0ae1028b8137d0032
Mohamed_Mugrabhi <mohamed.mamdouh.mugrabhi@gmail.com>
2026-01-25 10:35:59 +0200

    Create W. Shape3.cpp

---
+++ ICPC Assiut - Newcomers/Sheet #2  (Loops)/W. Shape3.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include<iomanip>
+using namespace std;
+int main()
+{
+ios_base::sync_with_stdio(0);
+cin.tie(0);
+cout.tie(0);
+
+int x;
+cin>>x;
+  
+for(int i=1;i<=x;i++){
+
+        for(int n=x;n>i;n--){
+            cout<<" ";
+        }
+    for(int j=1;j<=i*2-1;j++){
+        cout<<"*";
+    }
+    cout<<endl;
+}
+  
+for(int i=x;i>=1;i--){
+    for(int n=x;n>i;n--){
+        cout<<" ";
+    }
+     for(int j=1;j<=i*2-1;j++){
+        cout<<"*";
+    }
+    cout<<endl;
+}
+
+
+   return 0;
+} 

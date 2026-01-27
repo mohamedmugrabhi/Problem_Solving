47f7c7f1b62ec44d647371079681a4ea647dcf92
Mohamed_Mugrabhi <mohamed.mamdouh.mugrabhi@gmail.com>
2026-01-27 13:28:13 +0200

    Create B. Watermelon.cpp

---
+++ ICPC Assiut - Newcomers/Sheet #8 (General easy)/B. Watermelon.cpp
@@ -0,0 +1,20 @@
+#include <iostream>  
+ #include <string>
+  #include <cmath>
+   #include <algorithm>
+    #include <iomanip>
+using namespce std;
+int main() {
+
+short x;
+cin>>x;
+  
+if(x%2==0&&x>2){
+    cout<<"YES"<<endl;
+}
+else{
+    cout<<"NO"<<endl;
+}
+  
+    return 0; 
+}
